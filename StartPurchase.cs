265f52399763130c2f927e17476c8fe3d09ef7d4
vinothkanth <vinothkanth@5gindia.net>
2018-07-27 18:28:20 +0530

    Start Purchase Class

---
+++ StartPurchase.cs
@@ -0,0 +1,32 @@
+﻿//*************************
+//  Auther : Vinothkanth V
+//  Creation date :27 /7/ 2018
+//  
+//
+//  
+// 
+//
+//*************************
+
+/// <summary>
+/// The SpendingSpreeProgram Namespace
+/// </summary>
+
+namespace SpendingSpreeProgram
+{
+    using System;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// The StartPurchasing Class
+    /// </summary>
+    class StartPurchase
+    {
+        static void Main(string[] args)
+        {
+            // To start SpendingSpree
+            SpendingSpree.startPurchase();
+        }
+    }
+}
