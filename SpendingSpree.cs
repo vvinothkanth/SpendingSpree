8f3e4ceb637d14b1a84883a18d60cf2a25a8deec
vinothkanth <vinothkanth@5gindia.net>
2018-08-01 17:03:50 +0530

    Changes

--- SpendingSpree.cs
+++ SpendingSpree.cs
@@ -1,4 +1,4 @@
-﻿//*************************
+﻿//===================================
 //  Auther : Vinothkanth V
 //  Creation date :27 /7/ 2018
 //  
@@ -7,8 +7,7 @@
 //  to enter costs of items to be purchased, confirming every purchase
 //  The program should allow purchases to be made until there is no more money left. 
 //  The program should end when the user has no money left or has gone into debt from a single over purchase, 
-//
-//*************************
+// ===================================
 
 /// <summary>
 /// The SpendingSpreeProgram Namespace
@@ -25,69 +24,34 @@ namespace SpendingSpreeProgram
     /// </summary>
     public class SpendingSpree
     {
-        /// <summary>
-        /// Inititial Amount for user
-        /// </summary>
-        private static double userWallet = 1000;
-
+  
         /// <summary>
         /// Get and set the initial amount
         /// </summary>
-        public static double UserWallet
+        public double UserWallet
         {
-            get { return userWallet; }
-            set { userWallet = value; }
-        }        
-
-        /// <summary>
-        /// To inform the amount of purchasing 
-        /// </summary>
-        /// <returns>string of query</returns>
-        public static double costEntryQuery()
-        {
-            double userEnteredAmount = 0.0;
-            try
-            {
-                Console.Write("Enter the cost of the item that you want to buy $");
-                userEnteredAmount = Convert.ToDouble(Console.ReadLine());
-                if (userEnteredAmount <= 0)
-                {
-                    Console.WriteLine("Enter Valid Amount");
-                    startPurchase();
-                }
-            }
-            catch (ArgumentNullException argumentException)
-            {
-                throw new ArgumentNullException(argumentException.Message);
-            }
-
-            return userEnteredAmount;
+            get;
+            set;
         }
 
         /// <summary>
-        /// To ask and process the purchase amount
+        /// To initial amount for purchase to the customer
         /// </summary>
-        /// <param name="randomePrice">purchase Amount</param>
-        /// <returns>Wallet Balance</returns>
-        public static double getWalletAmount(double randomePrice)
+        /// <returns>return the initial amount</returns>
+        public double setInitialPurchaseAmount()
         {
+            double userHavingAmount = 0.0;
             try
             {
-                    if (purcheshConfirmation(randomePrice) == true)
-                    {
-                       UserWallet = withdrawOrDebuitAmount(randomePrice);
-                    }
-                    else
-                    {
-                        Console.WriteLine("Try Later...");
-                    }
+                userHavingAmount = Convert.ToDouble(Console.ReadLine());
+                UserWallet = userHavingAmount;
             }
-            catch (ArithmeticException cannotProcess)
+            catch (FormatException)
             {
-                throw new ArithmeticException(cannotProcess.Message);
+                Console.WriteLine("Your input is not in correct formate");
+                throw new FormatException();
             }
-
-            return UserWallet;
+            return userHavingAmount;
         }
 
         /// <summary>
@@ -95,7 +59,7 @@ namespace SpendingSpreeProgram
         /// </summary>
         /// <param name="randomePrice">PriceValue</param>
         /// <returns>true/false</returns>
-        public static bool checkWalletBalance(double priceValue)
+        public bool isUserHaveRequirAmount(double priceValue)
         {
             bool balanceStatus = false;
             try
@@ -116,31 +80,21 @@ namespace SpendingSpreeProgram
         /// <summary>
         /// Check the purchase is confirm or not
         /// </summary>
-        /// <param name="confirmamount">Confirm Amount</param>
+        /// <param name="confirmAmount">Confirm Amount</param>
         /// <returns>confirm status true/false</returns>
-        public static bool purcheshConfirmation(double confirmamount)
+        public  bool isPurchaseConfirm(string confirmAmount)
         {
             bool confirmStatus = false;
             try
             {
-                if (checkWalletBalance(confirmamount) == true)
-                {
-                    Console.WriteLine("Are you sure that you want to purchase the ${0} item ? (yes or no) ", confirmamount);
-                }
-                else
-                {
-                    Console.WriteLine("You don't have enough money to purchase that item. ");
-                    Console.WriteLine("Are you sure that you want to purchase the ${0} item and go into debt ?", confirmamount);
-                }
-                string confirmation = Convert.ToString(Console.ReadLine());
-                if (confirmation == "Yes" || confirmation == "yes")
+                if (confirmAmount == "Yes" || confirmAmount == "yes")
                 {
                     confirmStatus = true;
                 }
             }
-            catch (OperationCanceledException cancel)
+            catch (FormatException cancel)
             {
-                throw new OperationCanceledException(cancel.Message);
+                throw new FormatException(cancel.Message);
             }
 
             return confirmStatus;
@@ -151,52 +105,25 @@ namespace SpendingSpreeProgram
         /// </summary>
         /// <param name="purchasedAmount">Purchased Amount</param>
         /// <returns>Remaining amount in wallet</returns>
-        public static double withdrawOrDebuitAmount(double purchasedAmount)
+        public  double withdrawOrDebuitAmount(double purchasedAmount)
         {
             try
             {
-                if (checkWalletBalance(purchasedAmount) == true)
+                if (purchasedAmount < 0)
                 {
-                    UserWallet -= purchasedAmount;
+                    return purchasedAmount;
                 }
                 else
                 {
                     UserWallet -= purchasedAmount;
-                    Console.WriteLine("You have spent too much money and now owe: ${0}\n", UserWallet.ToString().Trim('-'));
-                    Console.WriteLine("Thankyou for Purchasing ...!!! \nVisit Again");
-                    Console.ReadKey();
-                    System.Environment.Exit(-1);
                 }
-
             }
-            catch (ArithmeticException amountIsNotValid)
+            catch (ArithmeticException)
             {
-                throw new ArithmeticException(amountIsNotValid.Message);
+                throw new ArithmeticException();
             }
 
             return UserWallet;
-        }
-
-        /// <summary>
-        /// Continue the purchase process until the user get debit amount .
-        /// </summary>
-        /// <returns>true</returns>
-        public static bool startPurchase()
-        {
-            try
-            {
-                Console.WriteLine("You have ${0} money remaining to spend. \n", SpendingSpree.UserWallet);
-                //double randomePrize = SpendingSpree.generateRamdomePrice();
-                double randomePrize = SpendingSpree.costEntryQuery();
-                double remainingAmount = SpendingSpree.getWalletAmount(randomePrize);
-                startPurchase();
-            }
-            catch (Exception porcessFlowException)
-            {
-                throw new Exception(porcessFlowException.Message);
-            }
-
-            return true;
         }
     }
 }
--- StartPurchase.cs
+++ StartPurchase.cs
@@ -26,7 +26,50 @@ namespace SpendingSpreeProgram
         static void Main(string[] args)
         {
             // To start SpendingSpree
-            SpendingSpree.startPurchase();
+            SpendingSpree spendingSpree = new SpendingSpree();
+
+            Console.Write("Please Enter how much purchase  amount you have $:");
+            double userWalletAmount = spendingSpree.setInitialPurchaseAmount();
+
+            while (spendingSpree.UserWallet > 0)
+            {
+                Console.WriteLine("You have ${0} money remaining to spend. \n", spendingSpree.UserWallet);
+                Console.Write("Enter the cost of the item that you want to buy $");
+                double purchaseAmount = Convert.ToDouble(Console.ReadLine());
+
+                if (purchaseAmount > 0)
+                {
+                    bool isHaveTheAmount = spendingSpree.isUserHaveRequirAmount(purchaseAmount);
+                    if (isHaveTheAmount)
+                    {
+                        Console.WriteLine("Are you sure that you want to purchase the ${0} item ? (yes or no) ", purchaseAmount);
+                    }
+                    else
+                    {
+                        Console.WriteLine("You don't have enough money to purchase that item. ");
+                        Console.WriteLine("Are you sure that you want to purchase the ${0} item and go into debt ?", purchaseAmount);
+                    }
+
+                    string purchaseConfirmation = Convert.ToString(Console.ReadLine());
+
+                    bool isConfirm = spendingSpree.isPurchaseConfirm(purchaseConfirmation);
+
+                    if (isConfirm)
+                    {
+                        double walletBallence = spendingSpree.withdrawOrDebuitAmount(purchaseAmount);
+                        if (walletBallence <= 0)
+                        {
+                            Console.WriteLine("You have spent too much money and now owe: ${0}\n", spendingSpree.UserWallet.ToString().Trim('-'));
+                            Console.WriteLine("Thankyou for Purchasing ...!!! \nVisit Again");
+                            Console.ReadKey();
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Enter Valid Amount");
+                }
+            }
         }
     }
 }
